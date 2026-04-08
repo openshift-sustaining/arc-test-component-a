eb3962158bd80eb040a7868f73a91a7b126e2f7a
Little Monster <rjagadal@redhat.com>
2026-04-08 17:10:07 +0530

    initial: Go 1.24 with grpc v1.78.0

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
