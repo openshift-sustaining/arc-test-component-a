869c2075bc558f90dd0e50fa113a543014ca5ecb
Little Monster <rjagadal@redhat.com>
2026-04-09 17:52:55 +0530

    initial: stale go.mod (Go 1.19 directive, deps need 1.22+)

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/openshift-sustaining/arc-test-component-a
+
+go 1.19
+
+require (
+	google.golang.org/grpc v1.55.0
+	golang.org/x/net v0.33.0
+)
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	_ "golang.org/x/net/http2"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
